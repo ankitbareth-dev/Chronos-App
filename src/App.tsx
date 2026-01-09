e36a21948459d2df9914c6d53267d747077a8f39
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-09 13:14:58 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      className="bg-brand-500 
+      hover:bg-brand-600 
+      text-white 
+      font-semibold 
+      py-3 px-6 
+     
+      transition-colors
+      duration-200
+    "
+    >
+      Chronos
+    </div>
+  );
+}
+
+export default App;
