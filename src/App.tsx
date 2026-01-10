f2cc9122b194ff605d71b130d0533ad36dc48cd5
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-10 12:06:33 +0530

    enable routing and auth page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Hero from "./features/landingPage/Hero";
 import Features from "./features/landingPage/Features";
@@ -5,16 +7,29 @@ import HowItWorks from "./features/landingPage/HowItWorks";
 import Testimonials from "./features/landingPage/Testimonials";
 import Footer from "./features/landingPage/Footer";
 
-function App() {
+import AuthPage from "./features/auth/Auth";
+
+function LandingPage() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Features />
       <HowItWorks />
       <Testimonials />
       <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
