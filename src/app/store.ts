7ebc1aa49a8f0129ade703d9f837a10ff5ecd13a
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-12 10:54:47 +0530

    added pretyped hooks for dispatch and selector to accsess them cleanly in the components

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,3 +3,9 @@ import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
   reducer: {},
 });
+
+export type AppStore = typeof store;
+
+export type AppDispatch = typeof store.dispatch;
+
+export type RootState = ReturnType<typeof store.getState>;
