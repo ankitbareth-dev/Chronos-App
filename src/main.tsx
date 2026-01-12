4629ccf264b0c6d54a828ea1c2775659b84c90d7
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-01-12 10:44:07 +0530

    provided store to entire app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { ENV } from "./config/env.ts";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={ENV.GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={ENV.GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
+    </Provider>
   </StrictMode>
 );
